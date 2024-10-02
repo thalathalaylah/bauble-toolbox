dc2c4a4b1e2e5efd89bd4c1d3208dd1ea3751bb9
thalathalaylah <3795604+thalathalaylah@users.noreply.github.com>
2024-10-02 23:06:00 +0900

    改行コードをLFに変更 & マルチウィンドウ試験

--- bauble-toolbox-gui/src-tauri/src/lib.rs
+++ bauble-toolbox-gui/src-tauri/src/lib.rs
@@ -4,11 +4,58 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri::{LogicalPosition, LogicalSize, WebviewUrl};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .setup(|app| {
+            let width = 800.;
+            let height = 600.;
+
+            let window = tauri::window::WindowBuilder::new(app, "main")
+                .inner_size(width, height)
+                .build()?;
+
+            let _webview1 = window.add_child(
+                tauri::webview::WebviewBuilder::new("main1", WebviewUrl::App(Default::default()))
+                    .auto_resize(),
+                LogicalPosition::new(0., 0.),
+                LogicalSize::new(width/2., height/2.),
+            )?;
+
+            let _webview2 = window.add_child(
+                tauri::webview::WebviewBuilder::new(
+                    "main2",
+                    WebviewUrl::External("https://github.com/tauri-apps/tauri".parse().unwrap()),
+                )
+                    .auto_resize(),
+                LogicalPosition::new(width / 2., 0.),
+                LogicalSize::new(width / 2., height / 2.),
+            )?;
+
+            let _webview3 = window.add_child(
+                tauri::webview::WebviewBuilder::new(
+                    "main3",
+                    WebviewUrl::External("https://tauri.app".parse().unwrap()),
+                )
+                    .auto_resize(),
+                LogicalPosition::new(0., height / 2.),
+                LogicalSize::new(width / 2., height / 2.),
+            )?;
+
+            let _webview4 = window.add_child(
+                tauri::webview::WebviewBuilder::new(
+                    "main4",
+                    WebviewUrl::External("https://twitter.com/TauriApps".parse().unwrap()),
+                )
+                    .auto_resize(),
+                LogicalPosition::new(width / 2., height / 2.),
+                LogicalSize::new(width / 2., height / 2.),
+            )?;
+
+            Ok(())
+        })
+        .run(tauri::generate_context!("./tauri.conf.json"))
+        .expect("error while running tauri application")
 }
