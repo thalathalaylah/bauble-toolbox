2dade0a8d497a0aae3a72669c5df87bfaf1cf08f
thalathalaylah <3795604+thalathalaylah@users.noreply.github.com>
2024-11-17 23:23:09 +0900

    [add] window設定をconfから読む

--- bauble-toolbox-gui/src-tauri/src/lib.rs
+++ bauble-toolbox-gui/src-tauri/src/lib.rs
@@ -5,42 +5,53 @@ fn greet(name: &str) -> String {
 }
 
 use tauri::{LogicalPosition, LogicalSize, WebviewUrl};
-
+use bauble_toolbox_logic::read_config;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
 pub fn run() {
-    tauri::Builder::default()
-        .setup(|app| {
-            let width = 1920.;
-            let height = 1080.;
+    // TODO: release buildの場合は適切な場所からconfを読む必要がある
+    match read_config("./config.json") {
+        Ok(_config) => {
+            // Configが読み込めた場合はアプリケーションをそのまま実行
+            tauri::Builder::default()
+                .setup(move |app| {
+                    let width = _config.window.width as f64;
+                    let height = _config.window.height as f64;
 
-            let left_window = width / 4.;
+                    let left_window = width / 4.;
 
-            let window = tauri::window::WindowBuilder::new(app, "main")
-                .inner_size(width, height)
-                .build()?;
+                    let window = tauri::window::WindowBuilder::new(app, "main")
+                        .inner_size(width, height)
+                        .build()?;
 
-            let _webview1 = window.add_child(
-                tauri::webview::WebviewBuilder::new(
-                    "main1",
-                    WebviewUrl::External("https://github.com/tauri-apps/tauri".parse().unwrap()),
-                )
-                    .auto_resize(),
-                LogicalPosition::new(0., 0.),
-                LogicalSize::new(left_window, height),
-            )?;
+                    let _webview1 = window.add_child(
+                        tauri::webview::WebviewBuilder::new(
+                            "main1",
+                            WebviewUrl::External(_config.window.side_url.parse().unwrap()),
+                        )
+                            .auto_resize(),
+                        LogicalPosition::new(0., 0.),
+                        LogicalSize::new(left_window, height),
+                    )?;
 
-            let _webview2 = window.add_child(
-                tauri::webview::WebviewBuilder::new(
-                    "main2",
-                    WebviewUrl::App(Default::default()),
-                )
-                    .auto_resize(),
-                LogicalPosition::new(left_window, 0.),
-                LogicalSize::new(width - left_window, height),
-            )?;
+                    let _webview2 = window.add_child(
+                        tauri::webview::WebviewBuilder::new(
+                            "main2",
+                            WebviewUrl::App(Default::default()),
+                        )
+                            .auto_resize(),
+                        LogicalPosition::new(left_window, 0.),
+                        LogicalSize::new(width - left_window, height),
+                    )?;
 
-            Ok(())
-        })
-        .run(tauri::generate_context!("./tauri.conf.json"))
-        .expect("error while running tauri application")
+                    Ok(())
+                })
+                .run(tauri::generate_context!("./tauri.conf.json"))
+                .expect("error while running tauri application");
+        }
+        Err(e) => {
+            // Configが読み込めなかった場合はエラーメッセージを表示して終了
+            eprintln!("Failed to read config: {}", e);
+        }
+    }
 }
